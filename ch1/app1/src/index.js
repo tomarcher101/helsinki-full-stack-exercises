a083225ea7907d79dfd373d2178081b8c7115f0f
Tom Archer <thomastarcher@icloud.com>
2020-04-01 21:46:16 +0100

    exercise 1.4

--- ch1/app1/src/index.js
+++ ch1/app1/src/index.js
@@ -3,27 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total part1={part1} part2={part2} part3={part3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -35,33 +34,41 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part1 part1={props.part1}/>
-      <Part2 part2={props.part2}/>
-      <Part3 part3={props.part3}/>
+      <Part1 part1={props.parts[0]}/>
+      <Part2 part2={props.parts[1]}/>
+      <Part3 part3={props.parts[2]}/>
     </>
   )
 }
 
-const Total = (props) => (
-  <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
-)
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
 
-const Part1 = (props) => (
+const Part1 = (props) => {
+  return (
     <p>
       {props.part1.name} {props.part1.exercises}
     </p>
-)
+  )
+}
 
-const Part2 = (props) => (
+const Part2 = (props) => {
+  return (
     <p>
       {props.part2.name} {props.part2.exercises}
     </p>
-) 
+  )
+}
 
-const Part3 = (props) => (
+const Part3 = (props) => {
+  return (
     <p>
       {props.part3.name} {props.part3.exercises}
     </p>
-) 
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
